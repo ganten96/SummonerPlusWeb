f8743cc0fcf9aa4b40fe682f64fd62c4e1383e05
Nick Ganter <nickganter91@gmail.com>
2015-07-11 11:10:54 -0500

    Initial project

---
+++ SummonerPlus/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SummonerPlus.Startup))]
+namespace SummonerPlus
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
