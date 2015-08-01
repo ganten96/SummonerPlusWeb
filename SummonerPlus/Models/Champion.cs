a49e3b0d1519bdea8381029355be45da7d8146f9
Nick Ganter <nickganter94@hotmail.com>
2015-08-01 10:29:40 -0500

    starting web api calls and champion class

---
+++ SummonerPlus/Models/Champion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+namespace SummonerPlus.Models
+{
+    public class Champion
+    {
+        [JsonProperty("id")]
+        public long ID { get; set; }
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("key")]
+        public string Key { get; set; }
+    }
+}
